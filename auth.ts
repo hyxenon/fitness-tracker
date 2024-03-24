dedd7a5ca2bedbc625e18b08ee31b4974489fe7b
Justine Edward Santos <hydxenonmain@gmail.com>
2024-03-24 20:32:15 +0800

    Adding Google and Github Oauth

--- auth.ts
+++ auth.ts
@@ -1,8 +1,21 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+
+declare module "@auth/core/types" {
+  interface User {
+    role: "ADMIN" | "TEACHER" | "STUDENT";
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    role: "ADMIN" | "TEACHER" | "STUDENT";
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +23,30 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
